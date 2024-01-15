1b297bb539fa82e1959049f293026512bae6ab66
Sean Derrow <sean.derrow@gmail.com>
2024-01-15 01:54:09 -0500

    Clean-up

--- cfautoupdater.py
+++ cfautoupdater.py
@@ -1,4 +1,5 @@
-import requests, time, json, configparser, smtplib, logging, datetime
+import os, sys, requests, time, json, configparser, smtplib, logging, datetime
+from email.mime.text import MIMEText
 
 # Reading the keys from the cfauth.ini file
 config = configparser.ConfigParser()
@@ -7,6 +8,9 @@ config.read('cfauth.ini')
 zone_id = config.get('tokens', 'zone_id')
 bearer_token = config.get('tokens', 'bearer_token')
 record_id = config.get('tokens', 'record_id')
+email_sender = config.get('tokens', 'email_sender')
+email_password = config.get('tokens', 'email_password')
+email_recipient = config.get('tokens', 'email_recipient')
 
 # Setting up the logger (a file where it records all IP changes)
 logging.basicConfig(level=logging.INFO, filename='ipchanges.log', format='%(levelname)s :: %(message)s')
@@ -17,6 +21,21 @@ headers = {
     "content-type": "application/json"
     }
 
+email_subject = "Sean's server - Cloudflare DNS IP Updated"
+email_to = [email_recipient]
+
+check_wait_time = 300
+curr_ip_wait_time = 10
+
+def send_email(body):
+    msg = MIMEText(body)
+    msg['Subject'] = email_subject
+    msg['From'] = email_sender
+    msg['To'] = ', '.join(email_to)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(email_sender, email_password)
+       smtp_server.sendmail(email_sender, email_to, msg.as_string())
+
 while True:
     # Getting the initial data of your A Record
     a_record_url = requests.get(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}", headers=headers)
@@ -24,40 +43,29 @@ while True:
 
     # This is the current IP that your chosen A record has been set to on Cloudflare
     current_set_ip = arecordjson['result']['content']
-
-    # This gets your current live external IP (whether that is the same as the A record or not)
-    currentip = requests.get("https://api.ipify.org?format=json")
-
-    # Status code should be 200, otherwise the API is probably down (this can happen quite a bit)
-    ipcheck_status = currentip.status_code
-
-    # Handling any API errors (otherwise we'd be trying to change the IP to some random HTML)
-    while ipcheck_status != 200:
-        time.sleep(10)
-        currentip = requests.get("https://api.ipify.org?format=json")
-        ipcheck_status = currentip.status_code
-
-    currentactualip = currentip.json()['ip']
-
+    
+    current_actual_ip = current_set_ip
+    
     # This loop checks your live IP every 5 minutes to make sure that it's the same one as set in your DNS record
-    while currentactualip == current_set_ip:
-        time.sleep(300) # Wait for 300 seconds (5 minutes)
+    while True:
         currentip = requests.get("https://api.ipify.org?format=json") # Then it checks if your IP is still the same or not.
         ipcheck_status = currentip.status_code
 
-    # Handling any API errors AGAIN
+        # Handling any API errors AGAIN
         while ipcheck_status != 200:
-            time.sleep(10)
+            time.sleep(curr_ip_wait_time)
             currentip = requests.get("https://api.ipify.org?format=json")
             ipcheck_status = currentip.status_code
 
-        currentactualip = currentip.json()['ip']
-
-    else: # If your live IP is NOT the same as the A Record's IP
-        pass
+        current_actual_ip = currentip.json()['ip']
+        
+        if current_actual_ip != current_set_ip:
+            break
+        
+        time.sleep(check_wait_time) # Wait for 300 seconds (5 minutes)
 
     # The "Payload" is what we want to change in the DNS record JSON (in this case, it's our IP)
-    payload = {"content": currentactualip}
+    payload = {"content": current_actual_ip}
 
     # Change the IP using a PATCH request
     requests.patch(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}", headers=headers, data=json.dumps(payload))
@@ -66,27 +74,11 @@ while True:
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     # LOG THE CHANGE
-    logging.info(f"{now} - IP change from {current_set_ip} to {currentactualip}")
+    logging.info(f"{now} - IP change from {current_set_ip} to {current_actual_ip}")
 
 
     # Sends an email to you to let you know everything has been updated.
-    # If you don't want this, just delete everything below this comment.
-
-    sender = 'sender@google.com'
-    receivers = ['receiver@example.com']
-
-    message = f"""From: Server <sender@google.com>
-    To: Your email <receiver@example.com>
-    Subject: DNS IP Updated
-
-    The server's IP has changed from {current_set_ip} to {currentactualip}.
-
-    The DNS records have been updated.
-    """
-
-    smtpObj = smtplib.SMTP('smtp.example.com', port=587)
-    smtpObj.connect("smtp.example.com", port=587)
-    smtpObj.ehlo()
-    smtpObj.starttls()
-    smtpObj.login("username", "password")
-    smtpObj.sendmail(sender, receivers, message)
+    message = f"The server's IP has changed from {current_set_ip} to {current_actual_ip}. The DNS records have been updated."
+    send_email(message)
+    
+    time.sleep(check_wait_time)
